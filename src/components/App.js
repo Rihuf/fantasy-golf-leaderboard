0e5e8000d61905b61fd7ef58ec87a40a29dc69d3
RITCHIE FESPERMAN <ritchiefesperman@RITCHIEs-MacBook-Pro.local>
2019-04-25 19:57:16 -0400

    add boilerplate and begin fleshing out navbar

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <header>
+      <div className="nav-bar">
+        <p className="header-text">The Salisbury Open</p>
+        <div className="hamburger">
+          <div className="hamburger-line" />
+          <div className="hamburger-line" />
+          <div className="hamburger-line" />
+        </div>
+      </div>
+    </header>
+  );
+}
