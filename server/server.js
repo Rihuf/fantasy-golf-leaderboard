130ed09bebd1ce6520d572b0619319eeb83977fa
RITCHIE FESPERMAN <ritchiefesperman@RITCHIEs-MacBook-Pro.local>
2019-05-14 11:48:15 -0400

    heroku deployment code implemented

--- server/server.js
+++ server/server.js
@@ -18,6 +18,19 @@ const golferRoutes = require("./routes/golfer");
 app.use("/player", playerRoutes);
 app.use("/golfer", golferRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log("Node.js listening on process.env.PORT or port " + 5000);
 });
