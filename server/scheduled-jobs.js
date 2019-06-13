0e09f409ba865f09c19a696e05b422a42737740d
RITCHIE FESPERMAN <ritchiefesperman@RITCHIEs-MacBook-Pro.local>
2019-06-13 18:46:16 -0400

    edit cronjob for usopen

--- server/scheduled-jobs.js
+++ server/scheduled-jobs.js
@@ -1,8 +1,8 @@
 var request = require("request");
 
 function hitAPIUrl() {
-  request("https://the-salisbury-open.herokuapp.com/golfer/get_pga_data");
-  // request("https://localhost:5000/golfer/get_pga_data");
+  request("https://the-salisbury-open.herokuapp.com/golfer/get_usopen_data");
+  // request("https://localhost:5000/golfer/get_usopen_data");
 }
 
 hitAPIUrl();
